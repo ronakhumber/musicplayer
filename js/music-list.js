af331986455ac6d73c495d20dd7715e495cfe86b
Ronak Sakhuja <ronaksakhuja@Ronaks-MacBook-Air.local>
2021-12-13 18:06:54 +0530

    Create dummy music list

---
+++ js/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+      name: "Song Name 1",
+      artist: "Ronak S",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Song Name 2",
+      artist: "Ronak S",
+      img: "music-2",
+      src: "music-1"
+    },
+    {
+      name: "Song Name 3",
+      artist: "Ronak S",
+      img: "music-3",
+      src: "music-1"
+    },
+    {
+      name: "Song Name 4",
+      artist: "Ronak S",
+      img: "music-4",
+      src: "music-1"
+    },
+    {
+      name: "Song Name 5",
+      artist: "Ronak S",
+      img: "music-5",
+      src: "music-1"
+    },
+]
