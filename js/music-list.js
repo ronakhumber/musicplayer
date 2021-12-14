d3ec47d1d53a9d45e4a201131003cc736dd32c4e
Ronak Sakhuja <ronaksakhuja@Ronaks-MacBook-Air.local>
2021-12-14 10:39:25 +0530

    replace song list path

--- js/music-list.js
+++ js/music-list.js
@@ -1,4 +1,4 @@
 var request = new XMLHttpRequest();
-request.open("GET", "../songList.json", false);
+request.open("GET", "songList.json", false);
 request.send(null)
 let allMusicOG = JSON.parse(request.responseText);
