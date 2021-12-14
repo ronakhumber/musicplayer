cd558997bd83038f89afe1c59a5cd1edc95f6973
Ronak Sakhuja <ronaksakhuja@Ronaks-MacBook-Air.local>
2021-12-14 10:35:19 +0530

    Read songs from file

--- js/music-list.js
+++ js/music-list.js
@@ -1,32 +1,4 @@
-let allMusicOG = [
-    {
-      name: "nong Name 1",
-      artist: "Ronak Sx",
-      img: "music-1",
-      src: "music-1"
-    },
-    {
-      name: "boadng Name 2",
-      artist: "Ronak S",
-      img: "music-2",
-      src: "music-2"
-    },
-    {
-      name: "aong Name 3",
-      artist: "Ronak Sx",
-      img: "music-3",
-      src: "music-3"
-    },
-    {
-      name: "zong Name 4",
-      artist: "Ronak S",
-      img: "music-4",
-      src: "music-4"
-    },
-    {
-      name: "fong Name 5",
-      artist: "Ronak S",
-      img: "music-5",
-      src: "music-5"
-    },
-]
+var request = new XMLHttpRequest();
+request.open("GET", "../songList.json", false);
+request.send(null)
+let allMusicOG = JSON.parse(request.responseText);
